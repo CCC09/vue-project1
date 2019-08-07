5ffdfe873e4695254c06cb86ab9c3f72be07f046
cxy <245266542@qq.com>
2019-08-07 21:25:25 +0800

    vue插槽和vue插件的封装

---
+++ src/components/my-test.vue
@@ -0,0 +1,24 @@
+<template>
+<div class='container'>
+    <div>head</div>
+    <div>
+        <slot name="a" :msg="msg">123</slot>
+    </div>
+    <div>
+        <slot name="b" txt='popo'>321</slot>
+    </div>
+    <div>foot</div>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '哈哈哈'
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'></style>
--- src/main.js
+++ src/main.js
@@ -11,12 +11,17 @@ import '@/styles/index.less'
 import router from '@/router'
 
 import axios from '@/api'
+
+import myPlugin from '@/components'
+
 Vue.prototype.$http = axios
 
 // 产品级别提示  不开启    在开发阶段 提示越详细越好 开发级别提示    上线阶段后选true 日志内容比较少 运行快
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+
+Vue.use(myPlugin)
 
 // main.js 入口文件的功能
 // 职责1：引入需要依赖的模块（js,包，css , 其他资源）
