d6acfb69cddf2b2b64fecbce3af05c74dc218f78
cxy <245266542@qq.com>
2019-08-10 21:48:07 +0800

    发布文章的路由及基础布局完成

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Welcome from '@/views/welcome'
 import Article from '@/views/article'
 import NotFound from '@/views/404'
 import Image from '@/views/image'
+import Publish from '@/views/publish'
 
 import store from '@/store'
 
@@ -40,6 +41,11 @@ const router = new VueRouter({
           path: 'image',
           name: 'image',
           component: Image
+        },
+        {
+          path: 'publish',
+          name: 'publish',
+          component: Publish
         }
       ]
     },
---
+++ src/views/publish/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <el-card>
+      <div slot="header" class="clearfix">
+        <my-bread>发布文章</my-bread>
+      </div>
+      <el-form label-width="100px">
+        <el-form-item label="标题：">
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item label="内容：">富文本</el-form-item>
+        <el-form-item label="封面：">
+          <el-radio-group v-model="articleForm.cover.type">
+            <el-radio :label="1">单图</el-radio>
+            <el-radio :label="3">三图</el-radio>
+            <el-radio :label="0">无图</el-radio>
+            <el-radio :label="-1">自动</el-radio>
+          </el-radio-group>
+          <div class="img-btn">
+              <img src="../../assets/images/avatar.jpg" alt="">
+          </div>
+        </el-form-item>
+        <el-form-item label="频道：">
+          <my-channel v-model="articleForm.channel_id"></my-channel>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">发布</el-button>
+          <el-button>存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      articleForm: {
+        channel_id: null,
+        cover: {
+          type: 1
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'></style>
